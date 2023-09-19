74daabaa9ad0a28cf30e97424623c6ab746859df
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-09-18 20:49:25 -0400

    ✨ Create new set

---
+++ create-new-set.js
@@ -0,0 +1,14 @@
+/**
+ * @template T
+ * @return {Set<T>}
+ */
+export const createSet = () => {
+  return new Set()
+}
+
+/** @type {Set<string>} */
+const stringSet = createSet()
+/** @type {Set<number>} */
+const numberSet = createSet()
+/** @type {Set<unkown>} */
+const unknownSet = createSet()
