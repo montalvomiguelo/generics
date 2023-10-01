430a0b25531e0032305960fb49febfded362e7a4
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-10-01 00:02:09 -0400

    ✨ Infer types from type arguments

---
+++ infer-types-from-type-arguments.js
@@ -0,0 +1,35 @@
+/**
+ * @template T
+ */
+export class Component {
+  /**
+   * @type {T}
+   * @private
+   */
+  props
+
+  /**
+   * @param {T} props
+   */
+  constructor(props) {
+    this.props = props
+  }
+
+  getProps() {
+    return this.props
+  }
+}
+
+/**
+ * @template T
+ * @param {Component<T>} component
+ */
+function cloneComponent(component) {
+  return new Component(component.getProps())
+}
+
+const component = new Component({ a: 1, b: 2, c: 3 })
+
+const clonedComponent = cloneComponent(component)
+
+const result = clonedComponent.getProps()
---
+++ infer-types-from-type-arguments.ts
@@ -0,0 +1,21 @@
+export class Component<TProps> {
+  private props: TProps
+
+  constructor(props: TProps) {
+    this.props = props
+  }
+
+  getProps() {
+    return this.props
+  }
+}
+
+const cloneComponent = <TProps>(component: Component<TProps>) => {
+  return new Component(component.getProps())
+}
+
+const component = new Component({ a: 1, b: 2, c: 3 })
+
+const clonedComponent = cloneComponent(component)
+
+const result = clonedComponent.getProps()
